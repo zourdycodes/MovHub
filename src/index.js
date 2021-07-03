838779fc48728db64aed8c9f0e52c2b2cba2d33a
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-07-03 07:58:15 +0700

    Applying PWA

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import "./styles/index.css";
 import App from "./app";
+import * as serviceWorkerRegistration from "./serviceWorkersRegistration";
 
 render(<App />, document.getElementById("root"));
+
+serviceWorkerRegistration.register();
